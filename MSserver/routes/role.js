a376de6571eedc06b68c735c5e4a1e75d4e515a4
tammenterho <terho.mikko@gmail.com>
2023-12-12 18:22:07 +0200

    getAll

--- MSserver/routes/role.js
+++ MSserver/routes/role.js
@@ -1,22 +1,21 @@
 // role.js
 import express from "express";
 import Role from "../models/Role.js";
+import {
+  createRole,
+  getAllRoles,
+  updateRole,
+} from "../controllers/role.controller.js";
 
 const router = express.Router();
 
-// Create a new role to DB
-router.post("/create", async (req, res, next) => {
-  try {
-    if (req.body.role && req.body.role !== "") {
-      const newRole = new Role(req.body);
-      await newRole.save();
-      return res.send("Role Created");
-    } else {
-      return res.status(400).send("Bad Request");
-    }
-  } catch (error) {
-    return res.status(500).send("Internal Server error");
-  }
-});
+// Create a new role to DB - logic in controller
+router.post("/create", createRole);
+
+//Update role in db - logic in controller
+router.put("/update/:id", updateRole);
+
+//Get all roles in db - logic in controller
+router.get("/roles", getAllRoles);
 
 export default router;
