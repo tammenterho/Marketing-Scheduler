7a8f7657edb98325bfb1f284a7ac9ca160cd0bf3
tammenterho <terho.mikko@gmail.com>
2023-12-13 12:25:42 +0200

    salted password bcrypt

--- MSserver/controllers/auth.controller.js
+++ MSserver/controllers/auth.controller.js
@@ -1,14 +1,17 @@
 import Role from "../models/Role.js";
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
   const role = await Role.find({ role: "User" });
+  const salt = await bcrypt.genSalt(10); // Only 3 lines and password is crypted
+  const hashPassword = await bcrypt.hash(req.body.password, salt); // Only 3 lines and password is crypted
   const newUser = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     userName: req.body.userName,
     email: req.body.email,
-    password: req.body.password,
+    password: hashPassword, // Only 3 lines and password is crypted
     roles: role,
   });
 
@@ -16,5 +19,3 @@ export const register = async (req, res, next) => {
   console.log("Registered!");
   return res.status(200).send("User registered!");
 };
-
-//Moi
