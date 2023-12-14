4792c2771bdc03d4b8361188c89c9b4c9946f105
tammenterho <terho.mikko@gmail.com>
2023-12-14 16:50:33 +0200

    forgot password skeleton

--- client/src/app/pages/forgot/forgot.component.ts
+++ client/src/app/pages/forgot/forgot.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forgot',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './forgot.component.html',
   styleUrls: ['./forgot.component.css'],
 })
-export default class ForgotComponent {}
+export default class ForgotComponent implements OnInit {
+  forgotForm!: FormGroup;
+
+  fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.forgotForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+    });
+  }
+
+  sendEmail() {
+    console.log(this.forgotForm.value);
+  }
+}
