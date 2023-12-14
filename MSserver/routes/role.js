96f651a29466b58f15ea00f13a911d12739f2fd2
tammenterho <terho.mikko@gmail.com>
2023-12-14 11:17:41 +0200

    protected features

--- MSserver/routes/role.js
+++ MSserver/routes/role.js
@@ -7,14 +7,15 @@ import {
   getAllRoles,
   updateRole,
 } from "../controllers/role.controller.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // Create a new role to DB - logic in controller
-router.post("/create", createRole);
+router.post("/create", verifyAdmin, createRole);
 
 //Update role in db - logic in controller
-router.put("/update/:id", updateRole);
+router.put("/update/:id", verifyAdmin, updateRole);
 
 //Get all roles in db - logic in controller
 router.get("/roles", getAllRoles);
--- MSserver/utils/verifyToken.js
+++ MSserver/utils/verifyToken.js
@@ -36,3 +36,4 @@ export const verifyAdmin = (req, res, next) => {
     }
   });
 };
+//fs
