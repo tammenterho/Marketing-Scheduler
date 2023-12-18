7709aafbb21db19a93d54ec1b81f5f7a9c97ab37
tammenterho <terho.mikko@gmail.com>
2023-12-18 15:56:05 +0200

    new campaign hide/show

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -9,4 +9,21 @@ import { CreateCampaignComponent } from 'src/app/components/create-campaign/crea
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  color: string = 'bg-lime-600';
+  text: string = 'Show';
+  showAddCampaign: boolean = false;
+
+  showCreate() {
+    this.showAddCampaign = !this.showAddCampaign;
+    console.log('nappi ' + this.showAddCampaign);
+
+    if (this.color === 'bg-lime-600' && this.text === 'Show') {
+      this.color = 'bg-red-600';
+      this.text = 'Hide';
+    } else {
+      this.color = 'bg-lime-600';
+      this.text = 'Show';
+    }
+  }
+}
