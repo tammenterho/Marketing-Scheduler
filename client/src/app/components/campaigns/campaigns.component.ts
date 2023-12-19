454f62bd5f03ddd2674264a871f7ff5860ccab63
tammenterho <terho.mikko@gmail.com>
2023-12-19 10:51:06 +0200

    campaigns skeleton

---
+++ client/src/app/components/campaigns/campaigns.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-campaigns',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './campaigns.component.html',
+  styleUrls: ['./campaigns.component.css']
+})
+export class CampaignsComponent {
+
+}
--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateCampaignComponent } from 'src/app/components/create-campaign/create-campaign.component';
+import { CampaignsComponent } from 'src/app/components/campaigns/campaigns.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CreateCampaignComponent],
+  imports: [CommonModule, CreateCampaignComponent, CampaignsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export default class HomeComponent {
+export default class HomeComponent implements OnInit {
   color: string = 'bg-lime-600';
   text: string = 'Show';
   showAddCampaign: boolean = false;
+  authService = inject(AuthService);
+  isLoggedIn: boolean = false;
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe((res) => {
+      this.isLoggedIn = this.authService.isLoggedIn();
+    });
+  }
 
   showCreate() {
     this.showAddCampaign = !this.showAddCampaign;
