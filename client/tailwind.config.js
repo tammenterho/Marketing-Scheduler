ab862a5c7e81f887f4ab443802caf04b68df56f8
tammenterho <terho.mikko@gmail.com>
2023-12-27 13:31:13 +0200

    login styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ["./src/**/*.{html,ts}"],
   theme: {
     screens: {
       xxs: "300px", // min-width
+      xs: "600px", // min-width
     },
     extend: {},
   },
