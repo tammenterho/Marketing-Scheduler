40e3eeb01acaae7c1438f4f42b1319e7b2025a9e
tammenterho <terho.mikko@gmail.com>
2024-03-11 12:06:03 +0200

    typo

--- server/models/Campaign.js
+++ server/models/Campaign.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const CampaignSchema = new mongoose.Schema({
   adType: Boolean,
-  creationdate: Date,
+  creationDate: Date,
   creator: String,
   company: String,
   owner: String,
